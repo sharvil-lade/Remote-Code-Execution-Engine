85b786af2c9521701f49f09505fca1819956137e
sharvil-lade <sharvillade9917@gmail.com>
2024-08-07 13:04:40 +0530

    removed unwanted files

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const testCases = require("./routes/testCase.js");
 app.use("/testCase", testCases);
 
 const server = app.listen(process.env.PORT || 3000, async function () {
-  await mongoose.connect("mongodb://127.0.0.1:27017/OnlineJudgeDataBase");
+  await mongoose.connect("mongodb://127.0.0.1:27017/RCE-Engine-Backend");
   var host = server.address().address;
   var port = server.address().port;
   console.log("Listening http://%s:%s", host, port);
